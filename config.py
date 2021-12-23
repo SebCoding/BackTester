738fcafc5eb9f0f4eed80da8932691456a2c1f55
Sebastien Vezina <seblife@pm.me>
2021-12-23 15:38:37 +0800

    Added comment into config.py.

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SUPPORTED_FILE_FORMATS = ['csv', 'xlsx']
 # Supported Exchange List
 SUPPORTED_EXCHANGES = ['Binance', 'ByBit']
 
-# Valid Intervals
+# Valid Intervals. Some intervals are not supported by some exchanges
 VALID_INTERVALS = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '1w']
 
 # Implemented Strategies
