b48acc9c622c2e303492b9ee7edb455ed7a006bd
Sebastien Vezina <seblife@pm.me>
2021-12-27 22:32:25 +0800

    Set ByBit2 for futures markets.

--- exchanges/ByBit2.py
+++ exchanges/ByBit2.py
@@ -18,18 +18,18 @@ class ByBit2(IExchange):
     USE_TESTNET = False  # Boolean True/False
 
     interval_map = {
-        "1m": "1m",
-        "3m": "3m",
-        "5m": "5m",
-        "15m": "15m",
-        "30m": "30m",
-        "1h": "1h",  # 1 Hour
-        "2h": "2h",  # 2 Hours
-        "4h": "4h",  # 4 Hours
-        "6h": "6h",  # 6 Hours
-        "12h": '12h',  # 12 Hours
-        "1d": "1d",
-        "1w": "1w"
+        "1m": "1",
+        "3m": "3",
+        "5m": "5",
+        "15m": "15",
+        "30m": "30",
+        "1h": "60",  # 1 Hour
+        "2h": "120",  # 2 Hours
+        "4h": "240",  # 4 Hours
+        "6h": "360",  # 6 Hours
+        "12h": '720',  # 12 Hours
+        "1d": "D",
+        "1w": "W"
     }
 
     def __init__(self):
@@ -45,6 +45,8 @@ class ByBit2(IExchange):
         else:
             # Mainnet
             self.bybit.set_sandbox_mode(False)
+        self.bybit.options['defaultType'] = 'future'
+        self.bybit.options['adjustForTimeDifference'] = False
         self.bybit.timeout = 30000  # number in milliseconds, default 10000
 
     def load_markets(self):
