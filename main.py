999dc1606593afdcd08e074ad6108bc539ffdaa3
Sebastien Vezina <seblife@pm.me>
2021-12-30 20:19:32 +0800

    Fixed details column output in statistics.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from strategies.MACD_X import MACD_X
 from strategies.ScalpEmaRsiAdx import ScalpEmaRsiAdx
 from strategies.ScalpEmaRsiAdx_X import ScalpEmaRsiAdx_X
 
+
 # Ignore warnings when reading xlsx file containing list of values for dropdown
 from stats import stats_utils
 
